26b4b918403ac6124f5fe26731d994cfe6b287a4
juapasan <juapasan@bancolombia.com.co>
2024-09-16 15:38:35 -0500

    se configura viewchild

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css'
+})
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag(newTag: string){
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+  }
+
+}
